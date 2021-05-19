28da60ee0bf4dec1a71c6455749b881ca113dbee
Björn Böing <b.boeing@online.de>
2021-05-19 14:34:56 +0200

    Reformat extraction folder structure to be less redundant

--- runner.py
+++ runner.py
@@ -3,7 +3,6 @@ import os
 from argparse import ArgumentParser
 from logging import getLogger
 from pathlib import Path
-from typing import Union
 
 from scrapy.crawler import CrawlerProcess
 
@@ -14,19 +13,21 @@ from utils.paths import OUTPUT_DIR
 class Runner:
     lang: str = ""
     target: str = ""
-    lang_dir: Path = None
+    version: str = ""
+    target_dir: Path = None
 
-    def __init__(self, lang: str, target: str) -> None:
+    def __init__(self, lang: str, target: str, version: str) -> None:
         self.lang = lang
         self.target = target
+        self.version = version
         self.logger = getLogger(__name__)
-        self.lang_dir = OUTPUT_DIR / lang
-        if not self.lang_dir.exists():
-            self.lang_dir.mkdir()
-        self.lang_dir = self.lang_dir.relative_to(os.path.dirname(os.path.realpath(__file__)))
+        self.target_dir = OUTPUT_DIR / target
+        if not self.target_dir.exists():
+            self.target_dir.mkdir()
+        self.target_dir = self.target_dir.relative_to(os.path.dirname(os.path.realpath(__file__)))
 
     def run(self) -> None:
-        feed_uri = self.__build_feed_uri()
+        feed_uri = self.target_dir / (self.lang + "_data.json")
         if feed_uri is None:
             return None
         if feed_uri.exists():
@@ -45,40 +46,27 @@ class Runner:
         self.logger.info("Starting {} crawler".format(self.target))
         self.logger.info("Output goes to '{}'".format(feed_uri))
         if self.target == "item":
-            process.crawl(ItemSpider, lang=self.lang)
+            process.crawl(ItemSpider, lang=self.lang, version=self.version)
         elif self.target == "npc":
-            process.crawl(NPCSpider, lang=self.lang)
+            process.crawl(NPCSpider, lang=self.lang, version=self.version)
         elif self.target == "object":
-            process.crawl(ObjectSpider, lang=self.lang)
+            process.crawl(ObjectSpider, lang=self.lang, version=self.version)
         elif self.target == "quest":
-            process.crawl(QuestSpider, lang=self.lang)
+            process.crawl(QuestSpider, lang=self.lang, version=self.version)
         elif self.target == "xp":
-            process.crawl(QuestXpSpider, lang=self.lang)
+            process.crawl(QuestXpSpider, lang=self.lang, version=self.version)
 
         process.start()
-
-    def __build_feed_uri(self) -> Union[Path, None]:
-        if self.target == "item":
-            feed_uri = self.lang_dir / "item_data.json"
-        elif self.target == "npc":
-            feed_uri = self.lang_dir / "npc_data.json"
-        elif self.target == "object":
-            feed_uri = self.lang_dir / "object_data.json"
-        elif self.target == "quest":
-            feed_uri = self.lang_dir / "quest_data.json"
-        elif self.target == "xp":
-            feed_uri = self.lang_dir / "quest_xp_data.json"
-        else:
-            self.logger.error("Unknown target '{}'".format(self.target))
-            return None
-        return feed_uri
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("-l", "--lang", help="The language you want to scrape. Default: 'en'",
                         type=str)
-    parser.add_argument("-t", "--target", help="The target you want to scrape. Either 'npc' or 'quest'. Default: 'npc'",
+    parser.add_argument("-t", "--target", help="The target you want to scrape. Possible values are 'npc', 'quest', 'item', 'object' and "
+                                               "'xp' . Default: 'npc'",
+                        type=str)
+    parser.add_argument("-v", "--version", help="The version of WoW Classic you want to scrape. Either 'tbc' or 'classic'. Default: 'tbc'",
                         type=str)
     args = parser.parse_args()
 
@@ -86,6 +74,8 @@ if __name__ == '__main__':
         args.lang = "en"
     if args.target is None:
         args.target = "npc"
+    if args.version is None:
+        args.version = 'tbc'
 
-    runner = Runner(args.lang, args.target)
+    runner = Runner(args.lang, args.target, args.version)
     runner.run()
